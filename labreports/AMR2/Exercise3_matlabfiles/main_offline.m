05dc9e1a7bbe76bd2bbe1c559590e69ace2d4b90
TomPeerdeman <tompeerdeman93@hotmail.com>
2013-11-13 15:11:13 +0100

    Offline main half working

---
+++ labreports/AMR2/Exercise3_matlabfiles/main_offline.m
@@ -0,0 +1,55 @@
+%LASERSCAN_TEST
+%
+%   Author Davide Scaramuzza - davide.scaramuzza@ieee.org
+%   ETH Zurich - April, 25, 2007
+
+%stop(vid);
+% Configure the object for manual trigger mode.
+%triggerconfig(vid, 'manual');
+
+% Now that the device is configured for manual triggering, call START.
+% This will cause the device to send data back to MATLAB, but will not log
+% frames to memory at this point.
+%start(vid)
+
+% -------------------------------------------------------------------------
+% MOST IMPORTANT PARAMETERS
+% -------------------------------------------------------------------------
+% Rmax = 160;%        Max detectable distance, set to 160 pixels in VGA images.
+%                     Rmax was already loaded when calling "calibrate_camera.m"
+% Rmin = 77;%         Min detectable distance in pixels in VGA image
+%                     Rmin was already loaded when calling "calibrate_camera.m"
+alpha = 95;%          Radial distortion coefficient, YOU MAY NEED TO TUNE THIS PARAMETER!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+height = 0.17;%       camera height in meters, YOU MAY NEED TO TUNE THIS PARAMETER!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+BWthreshold = 100;%   Threshold for segment the image into Black & white colors, YOU MAY NEED TO TUNE THIS PARAMETER!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+angstep = 1.0;%       Angular step of the beam in degrees
+axislimit = 0.8;%     Axis limit
+
+calibrate_camera_offline
+
+%Rmin = 75;            % Overwrite value from calibration step.
+
+figure;
+
+% -------------------------------------------------------------------------
+% MAIN
+% -------------------------------------------------------------------------
+for i=1:35
+    tic;%                               Start counting elapsed time
+    
+    %snapshot = getsnapshot(vid);%       Acquire image
+    snapshot = imflipud(tmpsnapshot);
+    [undistortedimg, theta] =imunwrap(snapshot, center, angstep, Rmax, Rmin);
+    BWimg = img2bw(undistortedimg, BWthreshold);
+    
+    rho = getpixeldistance(BWimg, Rmin);
+    
+    imagesc(snapshot);
+    hold on;
+    drawlaserbeam(center,theta,rho);
+    hold off;
+    
+% Compute the time per frame and effective frame rate.
+elapsedTime = toc;
+effectiveFrameRate = 1/elapsedTime
+end
--- labreports/AMR2/matlab/imageloader.m
+++ labreports/AMR2/matlab/imageloader.m
@@ -7,7 +7,7 @@ imagefiles = dir(strcat(folder, '/*.jpg'));
 nfiles = length(imagefiles);    % Number of files found
 
 for ii=1:nfiles
-   currentfilename = imagefiles(ii).name;
-   currentimage = imread(currentfilename);
+   currentfilename = imagefiles(ii).name
+   currentimage = imread(strcat(folder, currentfilename))
    images{ii} = currentimage;
 end
